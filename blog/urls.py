571a67d66410fac67b8b9936d5bc23f529d8e50e
SajjadTorki <sajjadtorki72@gmail.com>
2023-09-06 13:25:00 +0430

    change  list view and index
    Change

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import welcome_page
+from .views import welcome_page,blog_list_view
 
 urlpatterns = [
-    path('', welcome_page , name="index"),
+    path('', welcome_page , name="home_page"),
+    path('blog/',blog_list_view , name="blog_list")
 ]
--- blog/views.py
+++ blog/views.py
@@ -8,3 +8,8 @@ from django.shortcuts import render
 
 def welcome_page(request):
     return render(request , 'index.html')
+
+
+
+def blog_list_view(request):
+    return render(request , 'blog/blog_list.html')
